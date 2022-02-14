666ca981aa15399b233489712b5d4b9067cf166a
Hasan Mustafa <hasan.mustafa@zacco.com>
2022-02-14 13:12:06 +0100

    serve api with routes

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/go-rest-api/routers"
+)
+
+func main() {
+	router := routers.GetRoutes()
+	http.Handle("/", router)
+	http.ListenAndServe(":8080", router)
 }
