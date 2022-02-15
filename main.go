f3c162d8a837fe6eec39d8e182cf2728c4dec8f4
Hasan Mustafa <hasan.mustafa@zacco.com>
2022-02-15 13:25:35 +0100

    temp codes

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/go-rest-api/repositories"
 	"github.com/go-rest-api/routers"
 )
 
 func main() {
 	router := routers.GetRoutes()
 	http.Handle("/", router)
-	http.ListenAndServe(":8080", router)
+	//http.ListenAndServe(":8080", router)
+
+	product, err := repositories.GetProductByCategoryId("1")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if product != nil {
+		json, err := json.Marshal(product)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(json))
+	}
+
+	/* 	products, err := repositories.GetProducts()
+	   	if err != nil {
+	   		fmt.Println(err)
+	   	}
+	   	fmt.Println(products) */
 }
